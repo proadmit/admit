da77df1949fd3c9dd3d16e5bcfe20be4607ca9a8
proadmit <hello@admit.uz>
2025-04-07 23:52:14 -0400

    footer

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -214,12 +214,19 @@ export default function PrivacyPolicy() {
               To exercise any of these rights or if you have any privacy-related
               inquiries, please reach out to us at{" "}
               <a
+<<<<<<< Updated upstream
 
                 href="mailto:hello@admitapp.org"
                 className="text-blue-600 hover:text-blue-800"
               >
                 hello@admitapp.org
 
+=======
+                href="mailto:hello@AdmitApp.org"
+                className="text-blue-600 hover:text-blue-800"
+              >
+                hello@AdmitApp.org
+>>>>>>> Stashed changes
               </a>
               .
             </p>
@@ -238,12 +245,19 @@ export default function PrivacyPolicy() {
             <p className="text-gray-600">
               Email:{" "}
               <a
+<<<<<<< Updated upstream
 
                 href="mailto:hello@admitapp.org"
                 className="text-blue-600 hover:text-blue-800"
               >
                 hello@admitapp.org
 
+=======
+                href="mailto:hello@AdmitApp.org"
+                className="text-blue-600 hover:text-blue-800"
+              >
+                hello@AdmitApp.org
+>>>>>>> Stashed changes
               </a>
             </p>
             <p className="mt-6 text-gray-600">
